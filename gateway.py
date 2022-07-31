f493a165030322e51eae8649113ff8e438460f78
Hao Wu <hao.wu.au@gmail.com>
2022-07-31 22:18:52 +1000

    Gateway function

--- gateway.py
+++ gateway.py
@@ -14,7 +14,9 @@ class Gateway:
                 self.units = units
         else:
             self.units = []
-        self.usb = serial.Serial(port = '/dev/ttyUSB0', baudrate = 19200, parity = serial.PARITY_NONE, stopbits = 1, bytesize = 8, timeout = 0)
+        self.usb = serial.Serial(port = '/dev/ttyUSB0', baudrate = 19200, parity = serial.PARITY_NONE, \
+            stopbits = 1, bytesize = 8, timeout = 0)
+        # self.modbus = serial.Serial()
 
 
     def add_unit(self, unit):
@@ -33,21 +35,37 @@ class Gateway:
         self.units.clear()
 
     def writeRegister(self, address, register, value):
-        pass
+        bytes = b''
+        self.modbus.write(bytes)
 
-    def trigger(self, unit, lock=True):
-        pass
+    def trigger(self, id, lock=True):
+        unit = filter(lambda x: x.unit == id, self.units)
+        if unit is not None and unit.selected:
+            address = unit.address
+            register = unit.register
+            self.writeRegister(address, register, 0x100)
 
     def loop(self):
         while True:
             time.sleep(1)
             bytes = self.usb.read(100)
-            
             print(' '.join('{:02X}'.format(a) for a in bytes))
 
-    def isLock(self, bytes):
-        pass
+            id = self.getReleaseDoorUnit(bytes)
+            if id > 0:
+                self.trigger(id)
 
-    def isUnlock(self, bytes):
-        pass
+
+    def isDoorReleaseCommand(self, bytes):
+        # 55 AA 00 0C 00 02 01 00 01
+        return bytes[0] == 0x55 and bytes[1] == 0xaa and bytes[2] == 0x0 and bytes[3] == 0xc and \
+            bytes[4] == 0x0 and bytes[5] == 0x2 and bytes[6] == 0x1 and bytes[7] == 0x0 and bytes[8] == 0x1
+
+    def getReleaseDoorUnit(self, bytes):
+        if self.isDoorReleaseCommand(bytes):
+            return bytes[9]
+        else:
+            return -1        
+
+
 
